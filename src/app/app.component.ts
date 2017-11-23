7921a336a0ffbfb352837cadd1b3f6ab4c873f5b
Gea Cassinello <geacassi@publicisgroupe.net>
2017-11-23 18:09:57 -0500

    Optimization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  secretWord = false;
+  clickCounter = 0;
+  logStyles = false;
+  logs = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  showSecret() {
+    this.secretWord = !this.secretWord;
+    this.clickCounter++;
+    this.logs.push(this.clickCounter);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { CodeComponent } from './code/code.component';
-import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CodeComponent,
-    TasksComponent
   ],
   imports: [
     BrowserModule,
